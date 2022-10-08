8a4a90649740c59bf82a859f99008480e7c0095a
mindrash <name@example.com>
2022-10-08 17:39:29 -0500

    rtp

--- gif.sh
+++ gif.sh
@@ -18,16 +18,16 @@ for d in */ ; do
     echo "starting sq convert"
     convert -dispose background -layers OptimizeTransparency -delay $seconds ./$d/square/*.png +repage -loop 0 "./$d/square/sq-${timestamp}.gif"
     echo "starting sq mogrify"
-    #mogrify -layers 'optimize' -fuzz 1% "./out/fad-${timestamp}.gif"
+    mogrify -layers 'optimize' -fuzz 5% "./$d/square/sq-${timestamp}.gif"
     echo "starting sq resize"
-    convert "./$d/square/sq-${timestamp}.gif" -coalesce -resize 1000x1000 -layers optimize -loop 0 "./$d/square/sq-1000x1000-${timestamp}.gif"
+    convert "./$d/square/sq-${timestamp}.gif" -resize 1000x1000 "./$d/square/sq-1000x1000-${timestamp}.gif"
 
     echo "starting rc convert"
     convert -dispose background -layers OptimizeTransparency -delay $seconds ./$d/rectangle/*.png +repage -loop 0 "./$d/rectangle/rc-${timestamp}.gif"
     echo "starting rc mogrify"
-    #mogrify -layers 'optimize' -fuzz 1% "./out/fad-${timestamp}.gif"
+    mogrify -layers 'optimize' -fuzz 5% "./$d/rectangle/rc-${timestamp}.gif"
     echo "starting rc resize"
-    convert "./$d/rectangle/rc-${timestamp}.gif" -coalesce -resize 1000x1000 -layers optimize -loop 0 "./$d/rectangle/rc-1000x1000-${timestamp}.gif"
+    convert "./$d/rectangle/rc-${timestamp}.gif" -resize 1000x1000 "./$d/rectangle/rc-1000x1000-${timestamp}.gif"
 
     mv ./$d "./staged-$d"
 done
